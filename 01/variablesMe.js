68d43a27265d3b1a1963e83b135a415a0379e37b
Himanshi Sharma <himanshish456@gmail.com>
2024-03-16 11:13:47 +0530

    number, boolean and type inference

--- 01/variablesMe.js
+++ 01/variablesMe.js
@@ -1,2 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello Himanshi";
 console.log(greetings);
+// number
+// let userId: number = 334455.3;
+var userId = 334455.3;
+userId.toFixed();
+//boolean
+var isLoggendIn = false;
--- 01/variablesMe.ts
+++ 01/variablesMe.ts
@@ -1,5 +1,13 @@
 let greetings: string = "Hello Himanshi";
-
 console.log(greetings);
+
+// number
+// let userId: number = 334455.3;
+
+let userId = 334455.3;
+userId.toFixed();
+
+//boolean
+let isLoggendIn: boolean = false;
 
 export {};
