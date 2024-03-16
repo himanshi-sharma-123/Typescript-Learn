6f42d2a388173e258d9d0069a8d0c5b6f7a24727
Himanshi Sharma <himanshish456@gmail.com>
2024-03-16 11:35:38 +0530

    don't use any

--- 01/variablesMe.ts
+++ 01/variablesMe.ts
@@ -6,8 +6,17 @@ console.log(greetings);
 
 let userId = 334455.3;
 userId.toFixed();
-
+//typescript is smart enough to get all the values
 //boolean
 let isLoggendIn: boolean = false;
+
+//any
+// let hero;//(don't know what will come under into this it maybe number string so it will become any)
+let hero: string;
+function getHero() {
+  return "thor";
+}
+
+hero = getHero();
 
 export {};
