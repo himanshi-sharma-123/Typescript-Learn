1959d79c56e251f87e3f4d020c17d78ee9b7aa37
Himanshi Sharma <himanshish456@gmail.com>
2024-03-18 11:55:55 +0530

    arrays

---
+++ 02/myArray.ts
@@ -0,0 +1,15 @@
+const superHeroes: string[] = [];
+// const heroPower: number[] = [];
+const heroPower: Array<number> = [];
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+
+const MLmodels: number[][] = [[255, 255, 255], []];
+
+superHeroes.push("spiderman");
+heroPower.push(2);
+allUsers.push({ name: "h", isActive: false });
