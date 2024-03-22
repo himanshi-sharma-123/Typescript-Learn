5b5b188d561a8b8aa63ee57e7e8989ab3dbb81d4
Himanshi Sharma <himanshish456@gmail.com>
2024-03-22 11:40:27 +0530

    enums

---
+++ 02/myEnums.js
@@ -0,0 +1,9 @@
+"use strict";
+// enum SeatChoice {
+//   AISLE = "aisle",
+//   MIDDLE = "middle",
+//   WINDOW = "window",
+//   FOURTH = 0,
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+var hsSeat = "aisle" /* SeatChoice.AISLE */;
---
+++ 02/myEnums.ts
@@ -0,0 +1,19 @@
+// enum SeatChoice {
+//   AISLE = "aisle",
+//   MIDDLE = "middle",
+//   WINDOW = "window",
+//   FOURTH = 0,
+// }
+
+// const hsSeat = SeatChoice.AISLE;
+
+const enum SeatChoice {
+  AISLE = "aisle",
+  MIDDLE = "middle",
+  WINDOW = "window",
+  FOURTH = 0,
+}
+
+const hsSeat = SeatChoice.AISLE;
+
+export {};
