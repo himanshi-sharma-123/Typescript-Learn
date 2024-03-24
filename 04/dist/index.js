6aab6b4cde131e1e62e624e73c8f843948fcf614
Himanshi Sharma <himanshish456@gmail.com>
2024-03-24 20:38:02 +0530

    private public

--- 04/dist/index.js
+++ 04/dist/index.js
@@ -1,9 +1,18 @@
 "use strict";
+// class User {
+//   public email: string;
+//   name: string;
+//   private readonly city: string = "Himanshi";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
 class User {
     constructor(email, name) {
-        this.city = "Himanshi";
         this.email = email;
         this.name = name;
+        this.city = "Himanshi";
     }
 }
 const himanshi = new User("him@gmail.com", "Himanshi");
--- 04/src/index.ts
+++ 04/src/index.ts
@@ -1,11 +1,20 @@
+// class User {
+//   public email: string;
+//   name: string;
+//   private readonly city: string = "Himanshi";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
 class User {
-  email: string;
-  name: string;
   readonly city: string = "Himanshi";
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
-  }
+  constructor(
+    public email: string,
+    public name: string
+  ) // private userId: string
+  {}
 }
 
 const himanshi = new User("him@gmail.com", "Himanshi");
