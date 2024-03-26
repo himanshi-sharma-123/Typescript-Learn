f6e308cc4188dc3489e840790b4e66f29be6732a
Himanshi Sharma <himanshish456@gmail.com>
2024-03-26 11:00:29 +0530

    generics in array and arrow fns

--- 04/dist/myGenerics.js
+++ 04/dist/myGenerics.js
@@ -16,3 +16,12 @@ function identityFour(val) {
     return val;
 }
 identityFour({ brand: "a", type: 1 });
+function getSearchProducts(products) {
+    // do some databse operations
+    const myIndex = 3;
+    return products[myIndex];
+}
+const getMoreSearchProducts = (products) => {
+    const myIndex = 4;
+    return products[myIndex];
+};
--- 04/src/myGenerics.ts
+++ 04/src/myGenerics.ts
@@ -26,3 +26,14 @@ interface Bottle {
 }
 
 identityFour<Bottle>({ brand: "a", type: 1 });
+
+function getSearchProducts<T>(products: T[]): T {
+  // do some databse operations
+  const myIndex = 3;
+  return products[myIndex];
+}
+
+const getMoreSearchProducts = <T>(products: T[]): T => {
+  const myIndex = 4;
+  return products[myIndex];
+};
